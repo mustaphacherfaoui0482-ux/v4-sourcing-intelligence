f1989f5aa13aa6e475e0de53fe557813718c8ac6
mustaphacherfaoui0482-ux <mustaphacherfaoui0482@gmail.com>
2026-08-20 20:20:09 +0200

    Add V4 entities foundation

---
+++ data/entities.js
@@ -0,0 +1,9 @@
+// V4 Sourcing Intelligence entities
+export const entities = {
+  PRODUCT: 'product',
+  SUPPLIER: 'supplier',
+  OFFER: 'offer',
+  ANALYSIS: 'analysis',
+  DECISION: 'decision',
+  HISTORY: 'history'
+};
