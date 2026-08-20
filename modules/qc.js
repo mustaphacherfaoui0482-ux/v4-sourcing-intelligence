3794bb3e0dabe44a41b659aa33099d65feda6d45
mustaphacherfaoui0482-ux <mustaphacherfaoui0482@gmail.com>
2026-08-20 20:18:23 +0200

    Add V4 QC module

---
+++ modules/qc.js
@@ -0,0 +1,9 @@
+// V4 Sourcing Intelligence - Quality Control module
+export const QC_STATUS = [
+  "A commander",
+  "Commande en cours",
+  "Recu - a tester",
+  "Valide",
+  "A corriger",
+  "Refuse"
+];
